5fcc6d062a481112fd5f81b50722ef98e9c17726
raouf <djennaoui.raouf@gmail.com>
2023-09-12 15:35:12 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import NavigationBar from "./components/base/NavigationBar";
-import Footer from "./components/base/Footer";
-import MessageToast from "./components/base/MessageToast";
-import {Button} from "react-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "./redux-toolkit/store/store";
-import {openMessageToast} from "./redux-toolkit/slices/MessageToastSlice";
 import Login from "./components/base/Login";
-import SignUpForm from "./components/base/SignUpForm";
+import MessageToast from "./components/base/MessageToast";
 
 function App() {
     const dispatch = useDispatch();
@@ -18,8 +13,8 @@ function App() {
     return (
     <div className="App">
 
-        <NavigationBar/>
-
+        <Login/>
+<MessageToast/>
 
     </div>
   );
--- src/components/base/Login/Login.tsx
+++ src/components/base/Login/Login.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
 import Form from "react-bootstrap/Form";
-import cosider from "./cosider.jpg"
+import axios from 'axios';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../redux-toolkit/store/store";
 import {Code, openMessageToast} from "../../../redux-toolkit/slices/MessageToastSlice";
+import { Carousel } from "react-bootstrap";
 
 type LoginProps = {
   //
@@ -12,12 +13,12 @@ type LoginProps = {
 const Login: React.FC = () => {
   const dispatch = useDispatch();
   const { show,code,title,text } = useSelector((state: RootState) => state.messageToast);
-  const [email,setEmail]=useState<string>("");
+  const [username,setusername]=useState<string>("");
   const [password,setPassword]=useState<string>("");
   const [validated,setValidated]=useState<boolean>(false);
 
-  const handleEmailChange = (e:any) => {
-    setEmail(e.target.value);
+  const handleusernameChange = (e:any) => {
+    setusername(e.target.value);
   }
   const handlePasswordChange = (e:any) => {
     setPassword(e.target.value);
@@ -32,102 +33,128 @@ const Login: React.FC = () => {
     setValidated(true);
   };
 
-  const handleLogin = (e:any) => {
+  const handleLogin = async (e:any) => {
     isValid(e);
-    if(email !== "" && password !== ""){
-      dispatch(openMessageToast({code:Code.SUCCESS,title:"Login",text:"loggin sucessfull"}))
+    if(username !== "" && password !== ""){
+      let formData:any={
+        username:username,
+        password:password,
+        token:""
+      }
+      console.log(`${process.env.REACT_APP_API_BASE_URL}`)
+      await axios.post(`${process.env.REACT_APP_API_BASE_URL}/authentication/login/`,formData)
+          .then((response:any) => {
+            dispatch(openMessageToast({code:Code.SUCCESS,title:"login",text:"Login Successful"}))
+          })
+          .catch((error:any) => {
+            dispatch(openMessageToast({code:Code.ERROR,title:"login",text:"Wrong Username or Wrong Password "}))
+          });
+
     }else{
-      dispatch(openMessageToast({code:Code.ERROR,title:"login",text:"email or password is empty"}))
+      dispatch(openMessageToast({code:Code.ERROR,title:"login",text:"username or password is empty"}))
     }
 
   }
 
   return (
-      <div
-          className="d-flex d-xl-flex align-items-center align-items-xl-center"
-          style={{ width: "100%", height: "100%" }}
-      >
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-md-9 col-lg-12 col-xl-10">
-                <div className="card shadow-lg o-hidden border-0 my-5">
-                  <div className="card-body p-0">
-                    <div className="row">
-                      <div className="col-lg-6 d-none d-lg-flex">
-                        <div
-                            className="flex-grow-1 bg-login-image"
-                            style={{
-                              background:
-                                  'url('+cosider+') center / cover no-repeat'
-                            }}
-                        />
-                      </div>
-                      <div className="col-lg-6">
-                        <div className="p-5">
-                          <div className="text-center">
-                            <h4 className="text-dark mb-4">Welcome back!</h4>
-                          </div>
-                          <div className="user">
-                            <>
-                              <Form noValidate validated={validated} >
-                                <div className="mb-3">
-                                  <Form.Group  controlId="validationCustom01">
-                                    <Form.Control
-                                        required
-                                        type="text"
-                                        placeholder="Email"
-                                        value={email}
-                                        onChange={(e)=>handleEmailChange(e)}
-                                    />
-                                  </Form.Group>
-                                </div>
-                                <div className="mb-3">
-                                  <Form.Group  controlId="validationCustom02">
-                                    <Form.Control
-                                        required
-                                        type="Password"
-                                        placeholder="Password"
-                                        value={password}
-                                        onChange={(e)=>handlePasswordChange(e)}
-                                    />
 
-                                  </Form.Group>
-                                </div>
-                              </Form>
-                            </>
-                            <button
-                                className="btn btn-primary d-block btn-user w-100"
-                                type="submit"
-                                style={{ background: "#df162c", borderColor: "#df162c" }}
-                                onClick={(e)=>handleLogin(e)}
-                            >
-                              Login
-                            </button>
-                            <div className="mb-3">
-                              <div className="custom-control custom-checkbox small" />
-                            </div>
-                            <hr />
-                            <hr />
-                          </div>
-                          <div className="text-center">
-                            <a
-                                className="small"
-                                href="forgot-password.html"
-                                style={{ color: "#df162c" }}
-                            >
-                              Forgot Password?
-                            </a>
-                          </div>
-                        </div>
+      <div className="container">
+        <div
+            className="card shadow-lg o-hidden border-0 my-5"
+            style={{ height: 500 }}
+        >
+          <div className="card-body p-0">
+            <div className="row" style={{ height: "100%" }}>
+              <div
+                  className="col-lg-6 d-none d-lg-flex justify-content-lg-center align-items-lg-center"
+                  style={{
+                    background: "#ffffff",
+                    borderTopLeftRadius: 6,
+                    borderBottomLeftRadius: 6
+                  }}
+              >
+                <Carousel >
+                  <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h5>First slide label</h5>
+                      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+
+                </Carousel>
+
+
+
+              </div>
+
+
+              <div className="col-lg-6">
+                <div className="p-5" style={{ height: "100%" }}>
+                  <div className="text-center">
+                    <h4 className="text-dark mb-4">
+                      <strong>
+                        <em>Login</em>
+                      </strong>
+                    </h4>
+                  </div>
+
+                    <Form noValidate validated={validated} >
+                      <div className="mb-3">
+                        <Form.Group  controlId="validationCustom01">
+                          <Form.Control
+                              required
+                              type="text"
+                              placeholder="username"
+                              value={username}
+                              onChange={(e)=>handleusernameChange(e)}
+                          />
+                        </Form.Group>
                       </div>
+                      <div className="mb-3">
+                        <Form.Group  controlId="validationCustom02">
+                          <Form.Control
+                              required
+                              type="Password"
+                              placeholder="Password"
+                              value={password}
+                              onChange={(e)=>handlePasswordChange(e)}
+                          />
+
+                        </Form.Group>
+                      </div>
+                    </Form>
+
+                    <div className="mb-3">
+                      <div className="custom-control custom-checkbox small" />
                     </div>
+                    <button
+                        className="btn btn-primary d-block btn-user w-100"
+                        onClick={(e)=>handleLogin(e)}
+                    >
+                      Login
+                    </button>
+                    <hr />
+
+                  <div className="text-center" />
+                  <div className="text-center">
+                    <a className="small" href="#">
+                      Create an Account!
+                    </a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
+        </div>
       </div>
+
+
+
   );
 
 }
